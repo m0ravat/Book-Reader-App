0301c15ff992dd71705e593e10e6ea6d4d8d70a8
Muhammad Ravat <m0ravat736@gmail.com>
2024-07-06 16:18:31 +0100

    Classes edit

--- BookProject/jaavscript.js
+++ BookProject/jaavscript.js
@@ -1,16 +1,11 @@
-const myLibrary = [
-    new Book("Harry Potter 1"),
-    new Book("Harry Potter 2"),
-    new Book("Harry Potter 3")
-  ];
-  const container = document.querySelector(".cardGrid");
-  
-  function Book(name) {
-    this.name = name;
-    this.read = "Reading";
+const container = document.querySelector(".cardGrid");
+class Book{
+  constructor(name, read = "Reading") {
+    this.name=name;
+    this.read=read;
   }
   
-  Book.prototype.printBooks = function() {
+  static printBooks(){
     container.innerHTML = ""; // Clear previous entries
     for (let x = 0; x < myLibrary.length; x++) {
       const newCard = document.createElement("div");
@@ -21,16 +16,16 @@ const myLibrary = [
         newCard.style.backgroundColor = "black";
       }
       newCard.innerHTML = `
-        <h2> Name of Book: </h2> ${myLibrary[x].name} <br> 
+        <h2>Name of Book:</h2> ${myLibrary[x].name} <br> 
         <p>Status: ${myLibrary[x].read}</p>
-        <button onclick="bookInstance.removeCard(${x})">Remove Book</button>
-        <br> <button onclick="bookInstance.changeRead(${x})">Change Read Status</button>
+        <button onclick="Book.removeCard(${x})">Remove Book</button>
+        <br> <button onclick="Book.changeRead(${x})">Change Read Status</button>
       `;
       container.appendChild(newCard);
     }
-  };
-  
-  Book.prototype.addBookToLibrary = function() {
+  }
+
+  static addBook(){
     const bookNameInput = document.getElementById("bookName");
     const bookName = bookNameInput.value.trim();
     if (bookName) {
@@ -39,22 +34,28 @@ const myLibrary = [
       bookNameInput.value = ""; // Clear the input field
       this.printBooks(); // Update the display
     }
-  };
-  
-  Book.prototype.removeCard = function(index) {
+  }
+
+  static removeCard(index){
     myLibrary.splice(index, 1); // Remove the book at the specified index
     this.printBooks(); // Update the display
-  };
-  
-  Book.prototype.changeRead = function(index) {
-    // Toggle the read status between "Reading" and "Finished"
+  }
+  static changeRead(index){
     myLibrary[index].read = myLibrary[index].read === "Reading" ? "Finished" : "Reading";
     this.printBooks(); // Update the display
-  };
+  }
+}
+const myLibrary = [
+  new Book("Harry Potter 1")
+];
   
-  const bookInstance = new Book(); // Create an instance of Book
-  
-  
+document.addEventListener("DOMContentLoaded", () => {
+  // Add book button event listener
+  document.getElementById("addBookBtn").addEventListener("click", () => {
+    Book.addBook();
+  });
+  Book.printBooks();
+});
 
 
 
